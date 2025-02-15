8d9c9ea61b2e70b37ca6c4545f12a3ceaf9661a4
Guanjino <jjpereiranaranjo@gmail.com>
2025-02-15 12:09:07 +0100

    configurar dotenv

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const dotenv = require("dotenv")
 const { dbConnection } = require('./config/db')
 
-const app = express();
+const app = express()
+dotenv.config()
+
 const PORT = process.env.PORT 
 
 app.use(express.urlencoded({ extended: true }));
